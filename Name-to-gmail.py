d072b7464548ce5fce87457bef754564bf97a2fd
Blake-McCullough <66660752+Blake-McCullough@users.noreply.github.com>
2021-11-12 10:09:50 +0800

    Update Name-to-gmail.py

--- Name-to-gmail.py
+++ Name-to-gmail.py
@@ -1,3 +1,7 @@
+#Made By Blake McCullough
+#Discord - Spoiled_Kitten#4911
+#Github - https://github.com/Blake-McCullough/
+#Email - privblakemccullough@protonmail.com
 print("Made by Blake McCullough") #prints creators name
 continu = 2
 print("to stop the code type stop with no capitals")
