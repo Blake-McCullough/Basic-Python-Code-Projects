40c2cef7a1459d6c6cdae5da404d7168e9a5053a
Blake-McCullough <66660752+Blake-McCullough@users.noreply.github.com>
2021-11-12 10:09:26 +0800

    Update Prime-number-finder.py

--- Prime-number-finder.py
+++ Prime-number-finder.py
@@ -1,3 +1,7 @@
+#Made By Blake McCullough
+#Discord - Spoiled_Kitten#4911
+#Github - https://github.com/Blake-McCullough/
+#Email - privblakemccullough@protonmail.com
 # Python program to display all the prime numbers within an interval
 
 lower = int(input("Choose the lowest number to go to"))
